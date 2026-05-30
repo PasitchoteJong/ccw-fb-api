e9b5d482174c7b1add7615766d0b9cda63ee5e1e
PasitJong <jong.pasitchote@gmail.com>
2026-05-30 14:10:08 +0700

    prisma schema done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
 import express from 'express'
 import authRoute from './routes/auth.routes.js'
+import createHttpError from 'http-errors'
+
 
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
-app.use('/api/post', (req, res) => { res.send('post service') })
+app.use('/api/post', (req, res) => {
+    // console.log(x)
+    res.send('post service')})
 app.use('/api/comment', (req, res) => { res.send('comment service') })
 app.use('/api/like', (req, res) => { res.send('like service') })
 
-app.use('',(req,res)=>{
-    res.send('Not found')
+app.use((req,res,next)=>{
+
+    return next (createHttpError.NotFound('Resource not found'))
+
+    // res.status(404).json({
+    //     message : "Not found"
+    // })
 })
+
+app.use( (err,req,res,next) => {
+   console.error(err)
+   res.status(err.status || 500)
+   res.json({
+       status: err.status || 500,
+       message: err.message || 'Internal Server Error',
+ })
+})
+
 
 export default app
